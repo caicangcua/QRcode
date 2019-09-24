a832446004a07ac40ed9b5ce3ed2bde2c7b2e3cd
caicangcua <36462470+caicangcua@users.noreply.github.com>
2019-09-24 10:23:28 +0700

    Update ProductsController.js

--- api/controllers/ProductsController.js
+++ api/controllers/ProductsController.js
@@ -7,8 +7,9 @@ const util = require('util')
 //const table = 'products'
 const QRCode = require('qrcode')
 const bwipjs = require('bwip-js');
+var url = require('url');
 
-module.exports = {
+var self = module.exports = {
     get: (req, res) => {
 
         var d = new Date();
@@ -16,52 +17,7 @@ module.exports = {
         //res.json({ 'callapi': 'adu' });
 
 
-        //var bc = "[)>{RS}06{GS}12SA{GS}16S1{GS}V805233{GS}S192380001{GS}P646802400D{GS}Q20{GS}3QPCE{GS}H19238{GS}5D190826094{GS}{RS}{EOT}";
-        //var bc = "^MAC612SA{GS}16S1{GS}V805233{GS}S192380001{GS}P646802400D{GS}Q20{GS}3QPCE{GS}H19238{GS}5D190826094{GS}";
-        //var bc = "^MAC612SA16S1V805233S192380001P646802400DQ203QPCEH192385D190826094";
-        //bc = bc.replace(/{RS}/g, '^030');//     Dim rs As Char = Convert.ToChar(30)
-        //bc = bc.replace(/{GS}/g, '^029');
-        //bc = bc.replace(/{EOT}/g, '^004');
-        // *****************************************************************************************
-        //You can invoke Macro 06 by putting "^MAC6" as a prefix to your input
-        //data. For example:
-
-        //Data: ^MAC6remainderOfStructuredData
-        //Options: parsefnc dmre
-
-        //Macro 06 should cause a reader to emit a header of "[)>{RS}06{GS}" and
-        //trailer of "{RS}{EOT}" so you need only specify the structured data
-        //that goes in between.
-        // *****************************************************************************************
-        //https://groups.google.com/forum/#!msg/postscriptbarcode/G8Eq253g5Pw/aJ8z2IctAwAJ
-        var bc = "12SA{GS}16S1{GS}V805233{GS}S192380001{GS}P646802400D{GS}Q20{GS}3QPCE{GS}H19238{GS}5D190826094{GS}";
-        bc = bc.replace(/{RS}/g, String.fromCharCode(30));//     Dim rs As Char = Convert.ToChar(30)
-        bc = bc.replace(/{GS}/g, String.fromCharCode(29));
-        bc = bc.replace(/{EOT}/g, String.fromCharCode(4));
-
-
-        bwipjs.toBuffer({
-            parsefnc: true,
-            bcid: 'datamatrix',       // Barcode type
-            text: '^MAC6' + bc,    // Text to encode
-            backgroundcolor: 'FFFFFF',
-            scale: 2,               // 3x scaling factor
-        }, function (err, png) {
-            if (err) {
-                // Decide how to handle the error
-                // `err` may be a string or Error object
-            } else {
-                // `png` is a Buffer
-                // png.length           : PNG file length
-                // png.readUInt32BE(16) : PNG image width
-                // png.readUInt32BE(20) : PNG image height
-
-
-                res.json({ 'qrimg': 'data:image/png;base64,' + png.toString('base64') });
-      
-            }
-        });
-
+        
     },
     detail: (req, res) => {
         let sql = 'SELECT * FROM products WHERE id = ?'
@@ -80,23 +36,19 @@ module.exports = {
         })
     },
     store: (req, res) => {
-        let data = req.body;
+        var args = url.parse(req.url, true).query;
+        if (!args || !args.bcid) {
+            res.json({ 'qrimg': '' });
+        } else {
+            if (args.bcid == 'qrcode') {
+                self.QRcode(req, res);
+            } else if (args.bcid == 'datamatrix') {
+                self.DataMatrix(req, res);
+            } else {
+                res.json({ 'qrimg': '' });
+            }
+        }
 
-        var segs = [
-            { data: 'K71_15_ADA', mode: 'byte' },
-            { data: '440250', mode: 'numeric' },
-            { data: 'C_', mode: 'byte' },
-            { data: '2019.07.01', mode: 'alphanumeric' },
-            { data: '_AK', mode: 'byte' },
-            { data: '190726', mode: 'numeric' },
-            { data: '_005_', mode: 'byte' },
-            { data: 'FOLDING CUSHION ASSY LH MC J72K CAB', mode: 'alphanumeric' },
-            { data: '_3840', mode: 'byte' }
-        ]
-
-        QRCode.toDataURL(segs, { errorCorrectionLevel: 'L', version: 5, maskPattern: 4 }, function (err, url) {
-            res.json({ 'qrimg': url });
-        })
 
         //let sql = 'INSERT INTO products SET ?';
         //var d = new Date();
@@ -113,6 +65,96 @@ module.exports = {
         db.query(sql, [req.params.productId], (err, response) => {
             if (err) throw err
             res.json({ message: 'Delete success!' })
+        })
+    }
+    , DataMatrix: (req, res) => {
+        //var bc = "[)>{RS}06{GS}12SA{GS}16S1{GS}V805233{GS}S192380001{GS}P646802400D{GS}Q20{GS}3QPCE{GS}H19238{GS}5D190826094{GS}{RS}{EOT}";
+        //var bc = "^MAC612SA{GS}16S1{GS}V805233{GS}S192380001{GS}P646802400D{GS}Q20{GS}3QPCE{GS}H19238{GS}5D190826094{GS}";
+        //var bc = "^MAC612SA16S1V805233S192380001P646802400DQ203QPCEH192385D190826094";
+        //bc = bc.replace(/{RS}/g, '^030');//     Dim rs As Char = Convert.ToChar(30)
+        //bc = bc.replace(/{GS}/g, '^029');
+        //bc = bc.replace(/{EOT}/g, '^004');
+        // *****************************************************************************************
+        //You can invoke Macro 06 by putting "^MAC6" as a prefix to your input
+        //data. For example:
+
+        //Data: ^MAC6remainderOfStructuredData
+        //Options: parsefnc dmre
+
+        //Macro 06 should cause a reader to emit a header of "[)>{RS}06{GS}" and
+        //trailer of "{RS}{EOT}" so you need only specify the structured data
+        //that goes in between.
+        // *****************************************************************************************
+        //https://groups.google.com/forum/#!msg/postscriptbarcode/G8Eq253g5Pw/aJ8z2IctAwAJ
+        //var bc = "12SA{GS}16S1{GS}V805233{GS}S192380001{GS}P646802400D{GS}Q20{GS}3QPCE{GS}H19238{GS}5D190826094{GS}";
+
+        let data = req.body;
+        var bc = data[0];
+        bc = bc.replace(/{RS}/g, String.fromCharCode(30));//     Dim rs As Char = Convert.ToChar(30)
+        bc = bc.replace(/{GS}/g, String.fromCharCode(29));
+        bc = bc.replace(/{EOT}/g, String.fromCharCode(4));
+
+
+        bwipjs.toBuffer({
+            parsefnc: true,
+            bcid: 'datamatrix',       // Barcode type
+            text:  bc,    // Text to encode
+            backgroundcolor: 'FFFFFF',
+            scale: 2,               // 3x scaling factor
+        }, function (err, png) {
+            if (err) {
+                // Decide how to handle the error
+                // `err` may be a string or Error object
+                res.json({ 'qrimg': url });
+
+            } else {
+                // `png` is a Buffer
+                // png.length           : PNG file length
+                // png.readUInt32BE(16) : PNG image width
+                // png.readUInt32BE(20) : PNG image height
+
+
+                res.json({ 'qrimg': 'data:image/png;base64,' + png.toString('base64') });
+
+            }
+        });
+
+    }
+    , QRcode: (req, res) => {
+        let data = req.body;
+        var bc = data[0];//'K71_15_ADA440250C_2019.07.01_AK190726_006_FOLDING CUSHION ASSY LH MC J72K CAB_3840';//
+
+        var rawPart = bc.split('_ADA');
+        var K71_15_ADA = rawPart[0] + '_ADA';
+        bc = rawPart[1];
+        rawPart = bc.split('C_');
+        var _440250 = rawPart[0];
+        bc = rawPart[1];
+        rawPart = bc.split('_AK');
+        var _2019_07_01 = rawPart[0];
+        //
+        bc = rawPart[1];
+        rawPart = bc.split('_');
+
+        var partname = [];
+        for (var i = 2; i < rawPart.length - 1; i++) {
+            partname.push(rawPart[i]);
+        }
+
+        var segs = [
+            { data: K71_15_ADA, mode: 'byte' },
+            { data: _440250, mode: 'numeric' },
+            { data: 'C_', mode: 'byte' },
+            { data: _2019_07_01, mode: 'alphanumeric' },
+            { data: '_AK', mode: 'byte' },
+            { data: rawPart[0], mode: 'numeric' },
+            { data: '_' + rawPart[1] + '_', mode: 'byte' },
+            { data: partname.join('_'), mode: 'alphanumeric' },
+            { data: '_' + rawPart[ rawPart.length - 1], mode: 'byte' }
+        ]
+
+        QRCode.toDataURL(segs, { errorCorrectionLevel: 'L', version: 5, maskPattern: 4 }, function (err, url) {
+            res.json({ 'qrimg': url });
         })
     }
 }
